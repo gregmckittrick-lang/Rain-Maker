32b4a51aa01e124d4b75581c615f6a46c5eabfdc
gregmckittrick-lang <gregmckittrick@gmail.com>
2026-07-02 10:36:33 -0700

    Update check_rain.py

--- check_rain.py
+++ check_rain.py
@@ -30,7 +30,7 @@ from twilio.rest import Client
 LATITUDE = 33.7090
 LONGITUDE = -117.7590
 LOCATION_LABEL = "92620 (Irvine, CA)"
-RAIN_THRESHOLD = 1  # percent
+RAIN_THRESHOLD = -1  # percent
 
 OPEN_METEO_URL = (
     "https://api.open-meteo.com/v1/forecast"
